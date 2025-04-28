377b605bc839efd701155667959291b3e5ba3d79
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2025-04-28 21:01:29 +0200

    created counter component

--- where-its-at/src/Components/UI/Counter.jsx
+++ where-its-at/src/Components/UI/Counter.jsx
@@ -1,1 +1,23 @@
+import { useState } from "react";
 
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+  const result = () => {
+    return counter;
+  };
+  return (
+    <div>
+      <h1>Counter: {result()}</h1>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={decrement}>Decrementar</button>
+    </div>
+  );
+};
+
+export default Counter;
