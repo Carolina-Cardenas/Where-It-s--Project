f5f224b67620388d4bf7c9e17ca8e0409eb9a5db
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2025-05-02 14:22:46 +0200

    created landingPage

--- where-its-at/src/Pages/LandingPage.jsx
+++ where-its-at/src/Pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 function LandingPage() {
   return (
     <main className="landingPage">
-      <img src="../assets/logo.png" alt="Logo-imagen" />
+      <img src="../assets/logo.png" alt="Logo-imagentu" />
       <section className="landingPage__title">
         <h1>Where It's @ </h1>
         <h2>Ticketing made easy </h2>
